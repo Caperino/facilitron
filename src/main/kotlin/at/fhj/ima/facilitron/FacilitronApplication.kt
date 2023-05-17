42b0a08560d3cc193b60cda3c2a2d91f289e9dfa
stefanjb-it <stefanjb76@gmail.com>
2023-05-17 14:18:27 +0200

    Added totp test and qr code
    Added repos for qr code and totp token; still work in progress; only for admin users

--- src/main/kotlin/at/fhj/ima/facilitron/FacilitronApplication.kt
+++ src/main/kotlin/at/fhj/ima/facilitron/FacilitronApplication.kt
@@ -1,10 +1,16 @@
 package at.fhj.ima.facilitron
 
 import at.fhj.ima.facilitron.service.SecurityRoleService
+import dev.robinohs.totpkt.otp.HashAlgorithm
+import dev.robinohs.totpkt.otp.totp.TotpGenerator
+import dev.robinohs.totpkt.random.RandomGenerator
+import dev.robinohs.totpkt.recovery.RecoveryCodeGenerator
+import dev.robinohs.totpkt.secret.SecretGenerator
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import java.time.Duration
 
 @SpringBootApplication
 class FacilitronApplication{
@@ -22,6 +28,29 @@ class FacilitronApplication{
 
 		// ----- EMPLOYEES -----
 
+	}
+
+	@Bean
+	fun totpGenerator(): TotpGenerator {
+		val generator = TotpGenerator()
+		generator.codeLength = 6
+		generator.algorithm = HashAlgorithm.SHA1
+		generator.timePeriod = Duration.ofSeconds(20)
+		return generator
+	}
+
+	@Bean
+	fun recoveryCodeGenerator(): RecoveryCodeGenerator {
+		val generator = RecoveryCodeGenerator()
+		generator.blockLength = 5
+		return generator
+	}
+
+	@Bean
+	fun secretGenerator(): SecretGenerator {
+		val generator = SecretGenerator()
+		generator.randomGenerator = RandomGenerator(charPool = listOf('B', 'C', 'D'))
+		return generator
 	}
 
 }
--- src/main/kotlin/at/fhj/ima/facilitron/controller/PublicController.kt
+++ src/main/kotlin/at/fhj/ima/facilitron/controller/PublicController.kt
@@ -2,21 +2,42 @@ package at.fhj.ima.facilitron.controller
 
 import at.fhj.ima.facilitron.security.DefaultClaim
 import at.fhj.ima.facilitron.security.DefaultURL
+import dev.robinohs.totpkt.otp.totp.TotpGenerator
+import dev.robinohs.totpkt.otp.totp.timesupport.generateCode
+import dev.robinohs.totpkt.recovery.RecoveryCodeGenerator
+import dev.robinohs.totpkt.secret.SecretGenerator
+import io.github.g0dkar.qrcode.QRCode
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.core.io.ByteArrayResource
+import org.springframework.http.MediaType.IMAGE_PNG_VALUE
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import java.io.ByteArrayOutputStream
+import java.io.FileOutputStream
+import java.util.*
 
 @Controller
-class PublicController{
+class PublicController(
+    private val totpGenerator: TotpGenerator,
+    private val recoveryCodeGenerator: RecoveryCodeGenerator,
+    private val secretGenerator: SecretGenerator
+){
     @GetMapping(DefaultURL.PUBLIC_LANDING_URL)
     fun publicPage(
         model: Model,
         @RequestParam or:String?
     ):String{
+        val secret = secretGenerator.generateSecret(6)
+        println(secret)
+        val imageOut = ByteArrayOutputStream()
+        QRCode("otpauth://totp/Facilitron?secret="+secret.secretAsString).render().writeImage(imageOut)
+        val imageBytes = imageOut.toByteArray()
+        val image = Base64.getEncoder().encodeToString(imageBytes);
+        model.addAttribute("testCode",totpGenerator.generateCode(secret.secretAsByteArray))
+        model.addAttribute("qr", image)
         return "index"
     }
 
