f49cfd4eb2bb21c1d416870f89870684f123389d
einfachhuba <huba.csicsics@edu.fh-joanneum.at>
2023-05-20 16:56:10 +0200

    Quickfix

--- src/main/kotlin/at/fhj/ima/facilitron/controller/PublicController.kt
+++ src/main/kotlin/at/fhj/ima/facilitron/controller/PublicController.kt
@@ -17,7 +17,7 @@ class PublicController{
         model: Model,
         @RequestParam or:String?
     ):String{
-        return "newemployee"
+        return "index"
     }
 
     @GetMapping("/hidden")
