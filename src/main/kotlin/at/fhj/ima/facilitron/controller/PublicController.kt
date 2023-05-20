96f830a13b5181518e9497c1582a27826e761250
einfachhuba <huba.csicsics@edu.fh-joanneum.at>
2023-05-20 16:54:48 +0200

    Create New Employee FrontEnd finished

--- src/main/kotlin/at/fhj/ima/facilitron/controller/PublicController.kt
+++ src/main/kotlin/at/fhj/ima/facilitron/controller/PublicController.kt
@@ -17,7 +17,7 @@ class PublicController{
         model: Model,
         @RequestParam or:String?
     ):String{
-        return "index"
+        return "newemployee"
     }
 
     @GetMapping("/hidden")
