d9b9a79e0536e9dc5f0067cdde0819ca1590fd6e
stefanjb-it <stefanjb76@gmail.com>
2023-06-17 11:45:22 +0200

    Cleanup imports

--- src/main/kotlin/at/fhj/ima/facilitron/model/Category.kt
+++ src/main/kotlin/at/fhj/ima/facilitron/model/Category.kt
@@ -1,9 +1,6 @@
 package at.fhj.ima.facilitron.model
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 /**
  * Base Class for all categories
